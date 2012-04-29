d90af59419248b9c89e71623d281b3bb4643fa0b
Slawosz <slawosz@gmail.com>
2012-04-29 21:58:48 +0200

    add progress bar

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using ShakeGestures;
+using System.Windows.Threading;
 
 namespace PhoneApp1
 {
@@ -19,6 +20,9 @@ namespace PhoneApp1
         // Constructor
         Abacus abacus;
         public static bool shaked = false;
+        DispatcherTimer timer = new DispatcherTimer();
+        int level = 40;
+        int ticks = 0;
 
         public MainPage()
         {
@@ -29,6 +33,35 @@ namespace PhoneApp1
             ShakeGesturesHelper.Instance.MinimumRequiredMovesForShake = 3;
             ShakeGesturesHelper.Instance.Active = true;
             Container.Children.Add(abacus.GetContainer());
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500); //.FromSeconds(2);
+            timer.Tick += new EventHandler(TimerTick);
+
+            ProgressBar.Maximum = 100;
+            ProgressBar.Value = 0;
+        }
+
+        void StartGame()
+        {
+            timer.Start();
+        }
+
+        void StopGame()
+        {
+            timer.Stop();
+            ProgressBar.Value = 0;
+            
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            ticks++;
+            ProgressBar.Value = ticks * 2;
+            System.Diagnostics.Debug.WriteLine("timer");
+            if (ticks == level)
+            {
+                StopGame();
+            }
         }
 
         private void Instance_ShakeGesture(object sender, ShakeGestureEventArgs e)
@@ -50,6 +83,12 @@ namespace PhoneApp1
         private void PageTitle_Tap(object sender, GestureEventArgs e)
         {
             PageTitle.Text = abacus.GetValue().ToString();
+        }
+
+        private void Button_Tap(object sender, GestureEventArgs e)
+        {
+
+            StartGame();
         }
 
     }
